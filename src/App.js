c5d4c1e6d3c446b7ea0416c175ac44aed5277f9f
Ketan Shukla <ketankshukla@gmail.com>
2021-02-02 21:51:48 -0800

    renamed styled.css to index.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
-import "./styles.css";
+import React from 'react'
+import './index.css'
 // comment
 const App = () => {
-  return <div className="App">Vanilla React App!</div>;
-};
+  return <div className='App'>Vanilla React App!</div>
+}
 
-export default App;
+export default App
